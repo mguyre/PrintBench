ae15770f7dbc36e57dae0bfd6aaf0d6a69b8f607
Mike Guyre <mguyre@guyre.net>
2026-08-04 14:34:31 -0400

    -madded style to line

--- tests/test_line.py
+++ tests/test_line.py
@@ -1,4 +1,4 @@
-from printbench import Line, Point, Vector
+from printbench import Line, Point, StrokeStyle, Style, Vector
 
 
 def test_line_has_correct_length():
@@ -26,3 +26,19 @@ def test_line_midpoint():
     )
 
     assert line.midpoint() == Point(12, 24)
+
+
+def test_line_can_reference_a_style():
+    style = Style(
+        stroke_color="cyan",
+        stroke_width=0.137,
+        stroke_style=StrokeStyle.CENTERLINE,
+    )
+
+    line = Line(
+        Point(17, 31),
+        Point(59, 113),
+        style=style,
+    )
+
+    assert line.style is style
--- tests/test_style.py
+++ tests/test_style.py
@@ -1,4 +1,8 @@
-from printbench import Style
+from dataclasses import FrozenInstanceError
+
+import pytest
+
+from printbench import StrokeStyle, Style
 
 
 def test_new_style_defaults_to_none():
@@ -9,6 +13,36 @@ def test_new_style_defaults_to_none():
     assert style.stroke_style is None
 
 
-# def test_style_stores_values():
+def test_style_stores_values():
+    style = Style(
+        stroke_color="cyan",
+        stroke_width=0.137,
+        stroke_style=StrokeStyle.CENTERLINE,
+    )
 
-# def test_style_is_immutable():
+    assert style.stroke_color == "cyan"
+    assert style.stroke_width == 0.137
+    assert style.stroke_style is StrokeStyle.CENTERLINE
+
+
+def test_styles_with_same_values_are_equal():
+    style1 = Style(
+        stroke_color="cyan",
+        stroke_width=0.137,
+        stroke_style=StrokeStyle.CENTERLINE,
+    )
+
+    style2 = Style(
+        stroke_color="cyan",
+        stroke_width=0.137,
+        stroke_style=StrokeStyle.CENTERLINE,
+    )
+
+    assert style1 == style2
+
+
+def test_style_is_immutable():
+    style = Style()
+
+    with pytest.raises(FrozenInstanceError):
+        style.stroke_width = 0.137
